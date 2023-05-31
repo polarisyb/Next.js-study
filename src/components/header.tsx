797c6703708a61d2275afbfa16771b4a4c0889d5
polarisyb <polaris@acrosspace.com>
2023-05-31 14:05:26 +0900

    Header 컴포넌트 Link 태그 적용

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,9 +5,11 @@ export default function Header() {
     <>
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-xl">Next Study</span>
-          </a>
+          <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span className="ml-3 text-2xl">
+              Next Study
+            </span>
+          </Link>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             <Link href="/" className="mr-5 hover:text-gray-900">홈</Link>
             <Link href="/about-me" className="mr-5 hover:text-gray-900">자기소개</Link>
