6091fa6150bbfd312b1f16b4f57eef68b14754cc
polarisyb <polaris@acrosspace.com>
2023-05-31 13:12:11 +0900

    section 컴포넌트 수정

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,18 +1,5 @@
 export default function Section () {
   return(
-    <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Next.js & Tailwind</h1>
-          <p className="mb-8 leading-relaxed">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <div className="flex justify-center">
-            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Wellcome to My App</button>
-          </div>
-        </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"></div>
-      </div>
-    </section>
+    <>Section</>
   );
 };
