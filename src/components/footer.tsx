172fee2f9e67cf22d4358b0baab8efa61b9336f1
polarisyb <polaris@acrosspace.com>
2023-05-31 14:05:36 +0900

    Footer 컴포넌트 Link 태그 적용

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,17 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <>
       <footer className="text-gray-600 body-font">
         <div className="bg-gray-100">
           <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <span className="ml-3 text-xl">Next Study</span>
-            </a>
-            <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
+            <Link href='/' className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+              <span className="ml-3 text-xl">
+                Next Study
+              </span>
+            </Link>
+            <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2023 Tailblocks —
               <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
