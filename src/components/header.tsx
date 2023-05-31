4d0c3f0c05587f8c8e1a20a61b6848f92fe60424
polarisyb <polaris@acrosspace.com>
2023-05-31 17:15:51 +0900

    다크모드 footer, header, hero 컴포넌트 적용

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,25 @@
+'use client'
+
 import Link from "next/link";
+import DarkModeToggleButton from "./home/darkModeToggleButton";
 
 export default function Header() {
   return (
     <>
-      <header className="text-gray-600 body-font">
+      <header className="text-gray-800 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-2xl">
+            <span className="ml-3 text-2xl darkmode-span">
               Next Study
             </span>
           </Link>
-          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center font-bold darkmode-headerlink">
             <Link href="/" className="mr-5 hover:text-gray-900">홈</Link>
             <Link href="/about-me" className="mr-5 hover:text-gray-900">자기소개</Link>
             <Link href="/projects" className="mr-5 hover:text-gray-900">프로젝트</Link>
             <Link href="/contact" className="mr-5 hover:text-gray-900">연락하기</Link>
           </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+          <DarkModeToggleButton />
         </div>
       </header>
     </>
