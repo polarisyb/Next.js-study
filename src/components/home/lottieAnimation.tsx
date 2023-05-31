15d7c879b6745c1d2be377945d002106fb7a8224
polarisyb <polaris@acrosspace.com>
2023-05-31 17:16:23 +0900

    LottieAnimation 적용 (computer, developer, rocket)

--- src/components/home/lottieAnimation.tsx
+++ src/components/home/lottieAnimation.tsx
@@ -3,9 +3,9 @@
 import React from 'react'
 import Lottie from 'react-lottie-player'
 
-// import lottieJson from '../../public/computer.json'
-// import lottieJson from '../../public/developer.json'
-import lottieJson from '../../../public/rocket.json'
+import lottieJson from '../../../public/computer.json'
+// import lottieJson from '../../../public/developer.json'
+// import lottieJson from '../../../public/rocket.json'
 
 
 export default function LottieAnimation() {
