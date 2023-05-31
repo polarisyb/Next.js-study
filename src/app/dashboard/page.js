9136b93db45850f1458af358f4da2e28b9a3ffd2
polarisyb <polaris@acrosspace.com>
2023-05-31 10:27:53 +0900

    typescript 설치

--- src/app/dashboard/page.js
+++
@@ -1,9 +0,0 @@
-const dashboardHeader = <h1>Hello, dashboard!</h1>
-
-export default function Page() {
-  return (
-    <div>
-      {dashboardHeader}
-    </div>
-  );
-};
--- src/app/photo/page.js
+++
@@ -1,9 +0,0 @@
-const PhotoHeader = <h1>Hello, photo Page!</h1>;
-
-export default function photo() {
-  return (
-    <div>
-      {PhotoHeader}
-    </div>
-  );
-};
