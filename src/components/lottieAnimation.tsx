d8337b6657ec4f694a75f2f57d32773a656b9e2b
polarisyb <polaris@acrosspace.com>
2023-05-31 13:13:50 +0900

    lottieAnimation - rocket, computer, developer 파일 추가

---
+++ src/components/lottieAnimation.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react'
+import Lottie from 'react-lottie-player'
+
+// import lottieJson from '../../public/computer.json'
+// import lottieJson from '../../public/developer.json'
+import lottieJson from '../../public/rocket.json'
+
+
+export default function LottieAnimation() {
+  return (
+    <Lottie
+      loop
+      animationData={lottieJson}
+      play
+    />
+  )
+}
