934701df15c3fdf802af688d28d460215464edb5
polarisyb <polaris@acrosspace.com>
2023-06-01 18:19:57 +0900

    notion api 활용 연습 중

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,45 @@
-import Header from '@/components/header'
-import Footer from '@/components/footer'
+// projects 폴더 내의 page.tsx 파일의 코드
+import { Client } from '@notionhq/client';
 
-export default function Projects() {
-  return(
-    <h1>projects</h1>
+async function getData() {
+  const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+
+  try {
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID,
+    });
+
+    console.log(response);
+
+    return response.results;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export default function Projects({ projects }) {
+  if (!projects) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {projects.map((project) => (
+        <div key={project.id}>{project.name}</div>
+      ))}
+    </div>
   );
-};
+}
+
+export async function getStaticProps() {
+  const projects = await getData();
+
+  return {
+    props: {
+      projects: projects,
+    },
+  };
+}
